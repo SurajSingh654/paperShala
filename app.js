02e4c25d499ee9d3b423409109f1165a93a27a39
SurajSingh <surajsinghj654@gmail.com>
2022-02-05 15:22:56 +0530

    handle POST REQUEST in our dummy teacher data

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ const fs = require("fs");
 // Creates an Express application. The express() function is a top-level function exported by the express module.
 const app = express();
 
+// Express doesn't put body data in the request object explicitly...We have to add middleware i.e, app.use(express.json())
+app.use(express.json());
+
 // handle routes coming from the client
 // app.get("/", (req, res) => {
 //   res
@@ -17,12 +20,34 @@ const app = express();
 const teachers = JSON.parse(
   fs.readFileSync("./developmentData/api_data/teacher_apiData.json")
 );
+
+// get route handler
 app.get("/api/v1/teachers", (req, res) => {
   res.status(200).json({
     status: "success",
     totalData: teachers.length,
     data: { teachers },
   });
+});
+
+// post route handler
+app.post("/api/v1/teachers", (req, res) => {
+  console.log("Hello");
+  const newTeacher = req.body;
+  teachers.push(newTeacher);
+  console.log(newTeacher);
+  fs.writeFile(
+    "./developmentData/api_data/teacher_apiData.json",
+    JSON.stringify(teachers),
+    (err) => {
+      res.status(201).json({
+        status: "success",
+        data: {
+          newTeacher,
+        },
+      });
+    }
+  );
 });
 
 // Listen for connections.
