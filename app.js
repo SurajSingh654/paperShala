55f7a26658391f98dd8257db3b23ac6447045911
SurajSingh <surajsinghj654@gmail.com>
2022-02-09 20:15:44 +0530

    Add some security related npm packages

--- app.js
+++ app.js
@@ -2,6 +2,10 @@
 const express = require("express");
 const morgan = require("morgan");
 const rateLimit = require("express-rate-limit");
+const helmet = require("helmet");
+const mongoSanitize = require("express-mongo-sanitize");
+const xss = require("xss-clean");
+const hpp = require("hpp");
 const teacherRouter = require("./routes/teacherRoutes.js");
 const userRouter = require("./routes/userRoutes.js");
 const AppError = require("./utils/appError");
@@ -13,14 +17,21 @@ const app = express();
 
 // Middlewares
 // app-level middleware
-// Express doesn't put body data in the request object explicitly...We have to add middleware i.e, app.use(express.json())
-app.use(express.json());
-app.use((req, res, next) => {
-  console.log("Hello from the App-Level middleware💕🤗");
-  next();
-});
 
-// Morgan Middleware
+// SET SECURITY HTTP HEADERS middleware
+app.use(helmet());
+
+// BODY-PARSER{reading data from the body into req.body} middleware
+// Express doesn't put body data in the request object explicitly...We have to add middleware i.e, app.use(express.json())
+app.use(express.json({ limit: "20kb" }));
+
+// DATA SANITIZATION AGAINST NOSQL QUERY INJECTION
+app.use(mongoSanitize());
+
+// DATA SANITIZATION AGAINST XSS
+app.use(xss());
+
+// Development Logging middleware
 // Concise output colored by response status for development use. The :status token will be colored red for server error codes, yellow for client error codes, cyan for redirection codes, and uncolored for all other codes. :method :url :status :response-time ms - :res[content-length]
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
@@ -34,12 +45,9 @@ const limiter = rateLimit({
     "Too many requests from your side...Please try again after one hour!",
 });
 app.use("/api", limiter);
-// handle routes coming from the client
-// app.get("/", (req, res) => {
-//   res
-//     .status(200)
-//     .json({ status: "success", message: "Hello from the server!" });
-// });
+
+// TODO: ADD SOME FIELDS IN WHITELIST ARRAY
+app.use(hpp());
 
 // JSON.parse ==> A function that transforms the results. This function is called for each member of the object. If a member contains nested objects, the nested objects are transformed before the parent object is.
 
@@ -49,15 +57,6 @@ app.use("/api/v1/users", userRouter);
 
 // Handle all invalid routes
 app.all("*", (req, res, next) => {
-  // res.status(404).json({
-  //   status: "fail",
-  //   message: `Can't find ${req.originalUrl} on this server😑`,
-  // });
-
-  // Generate all invalid routes errors
-  // const error = new Error(`Can't find ${req.originalUrl} on this server😑`);
-  // error.status = "fail";
-  // error.statusCode = 404;
   next(new AppError(`Can't find ${req.originalUrl} on this server😑`, 404));
 });
 // Global Error Handler
